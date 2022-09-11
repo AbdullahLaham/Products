e60e9dc16f4fd9fdd1773a8dc502296040294bc5
AbdullahLaham <abed26194@gmail.com>
2022-09-11 12:36:07 +0300

    creating the folder structure

---
+++ youtube_clone/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <div className='color-red text-5xl'>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
